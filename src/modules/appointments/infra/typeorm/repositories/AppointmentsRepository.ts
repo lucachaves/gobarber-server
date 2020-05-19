0bfd240a440ed75bab0e94f42eed86b9bcad4d83
Luiz Chaves <lucachaves@gmail.com>
2020-05-19 15:42:46 -0300

    reescrevendo repositórios

--- src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
+++ src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
@@ -1,17 +1,33 @@
-import { EntityRepository, Repository } from 'typeorm';
+import { Repository, getRepository } from 'typeorm';
 
 import Appointment from '../entities/Appointment';
 import IAppointmentsService from '@modules/appointments/repositories/IAppointmentsService';
+import ICreateAppointmentDTO from '@modules/appointments/dtos/ICreateAppointmetDTO';
 
-@EntityRepository(Appointment)
-class AppointmentsRepository extends Repository<Appointment>
-  implements IAppointmentsService {
+class AppointmentsRepository implements IAppointmentsService {
+  private ormRepository: Repository<Appointment>;
+
+  constructor() {
+    this.ormRepository = getRepository(Appointment);
+  }
+
   public async findByDate(date: Date): Promise<Appointment | undefined> {
-    const findAppointment = await this.findOne({
+    const findAppointment = await this.ormRepository.findOne({
       where: { date },
     });
 
     return findAppointment || undefined;
+  }
+
+  public async crate({
+    provider_id,
+    date,
+  }: ICreateAppointmentDTO): Promise<Appointment> {
+    const appointment = await this.ormRepository.create({ provider_id, date });
+
+    await this.ormRepository.save(appointment);
+
+    return appointment;
   }
 }
 
