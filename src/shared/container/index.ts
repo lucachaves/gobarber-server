eb4650bbb7c6a6e5341ddc716dc4f47de3d8ee2f
Luiz Chaves <lucachaves@gmail.com>
2020-06-19 17:28:26 -0300

    criação de agendamento

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -3,8 +3,8 @@ import { container } from 'tsyringe';
 import '@modules/users/providers';
 import './providers';
 
-import IAppoitmentsRepository from '@modules/appointments/repositories/IAppointmentsRepository';
-import AppoitmentsRepository from '@modules/appointments/infra/typeorm/repositories/AppointmentsRepository';
+import IAppointmentsRepository from '@modules/appointments/repositories/IAppointmentsRepository';
+import AppointmentsRepository from '@modules/appointments/infra/typeorm/repositories/AppointmentsRepository';
 
 import IUsersRepository from '@modules/users/repositories/IUsersRepository';
 import UsersRepository from '@modules/users/infra/typeorm/repositories/UsersRepository';
@@ -12,9 +12,9 @@ import UsersRepository from '@modules/users/infra/typeorm/repositories/UsersRepo
 import IUserTokensRepository from '@modules/users/repositories/IUserTokensRepository';
 import UserTokensRepository from '@modules/users/infra/typeorm/repositories/UserTokensRepository';
 
-container.registerSingleton<IAppoitmentsRepository>(
-  'AppoitmentsRepository',
-  AppoitmentsRepository,
+container.registerSingleton<IAppointmentsRepository>(
+  'AppointmentsRepository',
+  AppointmentsRepository,
 );
 
 container.registerSingleton<IUsersRepository>(
