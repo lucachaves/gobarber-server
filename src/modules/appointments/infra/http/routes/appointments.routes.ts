1ae7767b86dd68ffd0a30b577f10d0124cb7f2c6
Luiz Chaves <lucachaves@gmail.com>
2020-06-20 20:27:41 -0300

    agendamento do prestador

--- src/modules/appointments/infra/http/routes/appointments.routes.ts
+++ src/modules/appointments/infra/http/routes/appointments.routes.ts
@@ -3,9 +3,11 @@ import { celebrate, Segments, Joi } from 'celebrate';
 
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
 import AppointmentsController from '../controllers/AppointmentsController';
+import ProviderAppointmentsController from '../controllers/ProviderAppointmentsControllers';
 
 const appointmentsRouter = Router();
 const appointmentsController = new AppointmentsController();
+const providerAppointmentsController = new ProviderAppointmentsController();
 
 appointmentsRouter.use(ensureAuthenticated);
 
@@ -19,5 +21,6 @@ appointmentsRouter.post(
   }),
   appointmentsController.create,
 );
+appointmentsRouter.get('/me', providerAppointmentsController.index);
 
 export default appointmentsRouter;
